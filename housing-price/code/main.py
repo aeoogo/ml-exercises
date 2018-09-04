c3643c90dbe65237859ea1d997bb2031092685c8
aeoogo <aeoogo@DESKTOP-QPHEVSQ>
2018-09-04 17:51:17 +0800

    补充数据集描述信息

--- housing-price/code/main.py
+++ housing-price/code/main.py
@@ -4,3 +4,22 @@ Created on 2018年9月4日
 
 @author: aeoogo
 '''
+# 导入需要的模块
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 用来绘图的，封装了matplot
+# 要注意的是一旦导入了seaborn，
+# matplotlib的默认作图风格就会被覆盖成seaborn的格式
+import seaborn as sns       
+
+from scipy import stats
+from scipy.stats import  norm
+from sklearn.preprocessing import StandardScaler
+import warnings
+warnings.filterwarnings('ignore')
+
+data_train = pd.read_csv("../data/train.csv")
+
+print(data_train)
