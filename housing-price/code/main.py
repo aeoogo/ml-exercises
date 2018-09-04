8b72e277d35d9226e14efacaad541ef47a157d0c
aeoogo <aeoogo@DESKTOP-QPHEVSQ>
2018-09-04 18:06:29 +0800

    解决seaborn不显示图片问题，末尾加入“plt.show()”即可

--- housing-price/code/main.py
+++ housing-price/code/main.py
@@ -8,12 +8,7 @@ Created on 2018年9月4日
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
-# 用来绘图的，封装了matplot
-# 要注意的是一旦导入了seaborn，
-# matplotlib的默认作图风格就会被覆盖成seaborn的格式
 import seaborn as sns       
-
 from scipy import stats
 from scipy.stats import  norm
 from sklearn.preprocessing import StandardScaler
@@ -22,4 +17,8 @@ warnings.filterwarnings('ignore')
 
 data_train = pd.read_csv("../data/train.csv")
 
-print(data_train)
+#print(data_train)
+print(data_train['SalePrice'].describe())
+sns.distplot(data_train['SalePrice'])
+plt.show()
+
